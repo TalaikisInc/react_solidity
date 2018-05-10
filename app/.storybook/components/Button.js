ae730891a0a0725333196cbb7c6d23ed83086196
xenu256 <talaikis.tadas@gmail.com>
2018-05-10 12:52:26 +0300

    adds storybook

---
+++ app/.storybook/components/Button.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Button from 'wix-style-react/Button';
+
+const Component = () =>
+  <Button onClick={() => console.log('thanks for clicking :)')}>
+    Click me!
+  </Button>
